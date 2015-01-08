f24ad4057e3d2d2842d0bed807478d17f29d18fb
euranium <mountrainier13@gmail.com>
2015-01-07 20:28:28 -0800

    piece adding to board for four square working

--- js/fourSquare.js
+++ js/fourSquare.js
@@ -1,4 +1,5 @@
-/*global $, jQuery, angular, module, scope */
+/*jslint plusplus: true*/
+/*global $, jQuery, angular, module, scope, swal, setItal, console, setCol, setColItal */
 
 var fourSquareCtrl = angular.module('fourSquareCtrl', []);
 
@@ -8,28 +9,50 @@ fourSquareCtrl.controller('fourListCtrl', ['$scope', '$http', function ($scope, 
 		$scope.board = data;
 	});
 	$scope.moves = 0;
-	$scope.message = 'here';
-	$scope.clearBoard = function () {
-		var i;
-		for (i = 0; i < 16; i++) {
-			this.loc[i].value = ' ';
+	$scope.currentPiece = '';
+	$scope.getPiece = function (pos) {
+		$scope.currentPiece = this.board.filled[pos];
+	};
+	$scope.setPiece = function (pos) {
+		if ($scope.currentPiece === '') {
+			return swal('Please select a piece to place first');
 		}
+		var temp = this.board.start[pos].pos;
+		console.log(this.board.start[pos]);
+		this.board.start[pos] = this.currentPiece;
+		this.board.start[pos].pos = temp;
+		this.currentPiece = '';
+	};
+	$scope.clearBoard = function () {
+		$http.get('js/json/four.json').success(function (data) {
+			$scope.board = data;
+		});
+		console.log('here');
 		this.moves = 0;
 	};
 	$scope.boardVal = function (val) {
-		if (val === null) {
+		if (!val) {
 			return ' ';
 		}
 		if (val.x === null) {
-			return this.car;
+			return ' ';
 		}
 		if (val.x === true) {
 			$scope.car = 'x';
 		} else {
-			$scope.car = 'o';
+			$scope.car = 'y';
 		}
 		if (val.cap === true) {
 			$scope.car = $scope.car.toUpperCase();
+		}
+		if ((val.italic === true) && (val.black === true)) {
+			setItal(val.pos);
+		}
+		if ((val.black === false) && (val.italic === false)) {
+			setCol(val.pos);
+		}
+		if ((val.black === false) && (val.italic === true)) {
+			setColItal(val.pos);
 		}
 		return $scope.car;
 	};
--- js/ticTac.js
+++ js/ticTac.js
@@ -1,13 +1,34 @@
 /*global $, jQuery, angular, $scope, console, swal, checkWin*/
+/*jslint plusplus: true*/
 
 Array.prototype.allSame = function () {
 	'use strict';
-	for(var i = 1; i < this.length; i++){
-        if(this[i] !== this[0])
-            return false;
+	var i;
+	for (i = 1; i < this.length; i++) {
+        if (this[i] !== this[0]) {
+			return false;
+		}
     }
 
     return true;
+};
+
+function setItal(poss) {
+	'use strict';
+	poss = '#' + poss;
+	$(poss).attr('style', 'font-style:italic; font-family: Georgia');
+}
+
+function setCol(poss) {
+	'use strict';
+	poss = '#' + poss;
+	$(poss).attr('style', 'color:orange;');
+}
+
+function setColItal(poss) {
+	'use strict';
+	poss = '#' + poss;
+	$(poss).attr('style', 'color:orange; font-style:italic; font-family: Georgia');
 }
 
 function checkWin(table) {
@@ -20,37 +41,37 @@ function checkWin(table) {
 	 */
 	'use strict';
 	var set = [table[0].value, table[1].value, table[2].value];
-	if ((table[0].value !== ' ') &&  (set.allSame() === true)){
+	if ((table[0].value !== ' ') &&  (set.allSame() === true)) {
 		return table[0].value;
 	}
 	set = [table[0].value, table[3].value, table[6].value];
-	if ((table[0].value !== ' ') &&  (set.allSame() ===true)){
+	if ((table[0].value !== ' ') &&  (set.allSame() === true)) {
 		return table[0].value;
 	}
 	set = [table[0].value, table[4].value, table[8].value];
-	if ((table[0].value !== ' ') &&  (set.allSame() === true)){
+	if ((table[0].value !== ' ') &&  (set.allSame() === true)) {
 		return table[0].value;
 	}
 	set = [table[1].value, table[4].value, table[7].value];
-	if ((table[1].value !== ' ') &&  (set.allSame() === true)){
+	if ((table[1].value !== ' ') &&  (set.allSame() === true)) {
 		return table[1].value;
-	} 
+	}
 	set = [table[2].value, table[5].value, table[8].value];
-	if ((table[2].value !== ' ') &&  (set.allSame() === true)){
+	if ((table[2].value !== ' ') &&  (set.allSame() === true)) {
 		return table[2].value;
-	} 
+	}
 	set = [table[2].value, table[4].value, table[6].value];
-	if ((table[2].value !== ' ') &&  (set.allSame() === true)){
+	if ((table[2].value !== ' ') &&  (set.allSame() === true)) {
 		return table[2];
-	} 
+	}
 	set = [table[3].value, table[4].value, table[5].value];
-	if ((table[3].value !== ' ') &&  (set.allSame() === true)){
+	if ((table[3].value !== ' ') &&  (set.allSame() === true)) {
 		return table[3].value;
-	} 
+	}
 	set = [table[6].value, table[7].value, table[8].value];
-	if ((table[6].value !== ' ') &&  (set.allSame() === true)){
+	if ((table[6].value !== ' ') &&  (set.allSame() === true)) {
 		return table[6].value;
-	} 
+	}
 	return ' ';
 }
 
